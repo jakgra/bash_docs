37377169532a5fb8b226b73667bd51d29725073f
Jakob Socan <10517617+jakgra@users.noreply.github.com>
2024-03-13 15:09:55 +0100

    Added perl example

--- grep_and_sed_advanced_replace.sh
+++ grep_and_sed_advanced_replace.sh
@@ -1,4 +1,7 @@
 grep -rl SOME_MACRO include | xargs -n 1 sed -i -e 's|SOME_MACRO\(.*\)macroArgN\(.*\)macroArgM\(.*\)marcoArgO\(.*\)|SOME_MACRO\1Prefix::macroArgN\2Prefix::macroArgM\3Prefix::macroArgO\4|'
 
+# multiline using perl instead of sed:
+grep -rl 'To parse this JSON data' some/directory/ | xargs -n 1 perl -0777 -i -pe 's/\/\/  To parse this JSON data.*\#pragma once/\#pragma once/s'
+
 
 # maybe fix line endings with the fix_git_line_endings_windows.sh script in this folder
